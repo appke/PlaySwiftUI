f47a84e186ec2c5358574fcec45a583d00632b68
mul2 <mul2@bngrp.com>
2020-04-07 19:58:50 +0800

    View对齐

--- playSwiftUI/ViewController.swift
+++ playSwiftUI/ViewController.swift
@@ -9,49 +9,60 @@
 import UIKit
 import SwiftUI
 
-
-//class ViewController: UIViewController {
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        // Do any additional setup after loading the view.
-//        Button(action: {
-//            print("-----1")
-//
-//        }) {
-//            Text("Buttonaaa")
-//        }
-//    }
-//}
-
-
+// Text对齐
 struct ContentView : View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("123abcde----11")
-                .font(.largeTitle)
-                .foregroundColor(Color.red)
-            HStack {
-                Text("水平方向----111")
-                    .font(.subheadline)
-                Spacer()
-                Text("水平方向----222")
-                    .font(.subheadline)
-                
+        HStack {
+            VStack(alignment: .trailing) {
+                Text("Username").border(Color.red)
+                Text("Email")
+                Text("Phone")
             }
+            .font(Font.system(size: 35, weight: .bold))
+            
+            VStack(alignment: .leading) {
+                Text("Jackay").frame(maxHeight: .infinity).border(Color.red)
+                Text("Jackay@gmail.com").frame(maxHeight: .infinity)
+                Text("400-2828-897").frame(maxHeight: .infinity)
+            }
+            .font(.system(size: 25))
         }
-        .padding()
+    .fixedSize()
+    }
+}
+
+// SF字体
+struct SF_ContentView : View {
+    let name = "sun.max"
+    var body: some View {
+        VStack(spacing: 45) {
+            Image(systemName: name) //SF系统自带图片
+            .resizable()
+            .scaledToFit()
+            .frame(width: 60, height: 60)
+            
+            Image(systemName: name)
+            .resizable()
+            .scaledToFit()
+            .frame(width: 90, height: 90)
+
+            
+            Image(systemName: name)
+            .resizable()
+            .scaledToFit()
+            .frame(width: 120, height: 120)
+
+        }
     }
 }
 
 
 
-#if DEBUG
+
 struct ViewController_Previews: PreviewProvider {
     static var previews: some View {
-        //Text("Hello, World!333")
-        ContentView()
-                
+        //Text("Hello, World!333").font(Font.system(size: 35))
+//        ContentView()
+        SF_ContentView()
     }
 }
-#endif
