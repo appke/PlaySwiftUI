f6b06ab717f86f6d231dc07d66b04d414758d940
mul2 <mul2@bngrp.com>
2020-04-08 19:15:11 +0800

    阴影杂谈

--- playSwiftUI/ViewController.swift
+++ playSwiftUI/ViewController.swift
@@ -88,13 +88,72 @@ struct SF_ContentView : View {
     }
 }
 
+//MARK:- 阴影使用
+struct ShadowView : View {
+    var body: some View {
+        VStack {
+            Spacer()
+            
+            VStack {
+                Text("Text 11")
+                Text("Text 22")
+                //.shadow(color: Color.red, radius: 10, x: 0, y: 0)
+            }
+            .font(.system(size: 35))
+            .padding()
+            .background(Color.white)
+            .clipped() //带阴影的矩形
+            .shadow(color: .red, radius: 10)
+            
+//            .background(
+//                //Color.white.shadow(color: .red, radius: 10, x: 0, y: 0)
+//                RoundedRectangle(cornerRadius: 20)
+//                    .foregroundColor(.white)
+//                    .shadow(color: .red, radius: 10)
+//
+//            )
+            
+            Spacer()
+            
+            
+            VStack {
+                Text("Text 33")
+                Text("Text 44")
+                //.shadow(color: Color.red, radius: 10, x: 0, y: 0)
+            }
+            .font(.system(size: 35))
+            .padding()
+            .background(Color.white)
+            .cornerRadius(10) //带阴影的圆角矩形
+            .shadow(color: .red, radius: 10)
 
+            
+            Spacer()
+            
+            
+            VStack {
+                Text("Text 55")
+                Text("Text 66")
+                //.shadow(color: Color.red, radius: 10, x: 0, y: 0)
+            }
+            .font(.system(size: 35))
+            .padding()
+            .background(Color.white)
+            .cornerRadius(10)
+            .shadow(radius: 10, y:5)
+            // 实际项目，透明黑色、带圆角
+        
+            Spacer()
+        }
+    }
+}
 
 struct ViewController_Previews: PreviewProvider {
     static var previews: some View {
         //Text("Hello, World!333").font(Font.system(size: 35))
 //        ContentViewV()
 //        SF_ContentView()
-        ContentViewH()
+//        ContentViewH()
+        ShadowView()
     }
 }
