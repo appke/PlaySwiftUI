ace1f1e7cc995d720f98253aa9b1079ffd69b0fb
appke <liangmu.gdut@gmail.com>
2020-04-15 09:23:31 +0800

    动画框架

---
+++ DrawAnimation/HikeView.swift
@@ -0,0 +1,21 @@
+//
+//  HikeView.swift
+//  DrawAnimation
+//
+//  Created by MGBook on 2020/4/14.
+//  Copyright © 2020 MGBook. All rights reserved.
+//
+
+import SwiftUI
+
+struct HikeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HikeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HikeView()
+    }
+}
