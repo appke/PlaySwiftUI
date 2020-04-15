02929a26c3f936b27af78d91ca437b1bdb465e40
appke <liangmu.gdut@gmail.com>
2020-04-15 12:52:01 +0800

    初步动画效果

--- DrawAnimation/HikeView.swift
+++ DrawAnimation/HikeView.swift
@@ -9,13 +9,67 @@
 import SwiftUI
 
 struct HikeView: View {
+    var hike: Hike
+    @State private var showDetail = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    
+        VStack {
+            HStack {
+                HikeGraph(hike: hike, path: \.elevation)
+                    .frame(width: 50, height:30)
+                
+                VStack(alignment: .leading) {
+                    Text(hike.name)
+                        .font(.headline)
+                    Text(hike.distanceText)
+                    
+                }
+                Spacer()
+                
+                Button(action: {
+                    //self.showDetail.toggle()
+                    withAnimation (.spring()) {
+                        self.showDetail.toggle()
+                    }
+                }) {
+                    Image(systemName: "chevron.right.circle")
+                        .imageScale(.large)
+                        .rotationEffect(.degrees(showDetail ? 90 : 0))
+                        //.animation(nil)
+                        .scaleEffect(showDetail ? 1.5 : 1)
+                        .padding()
+                        //.animation(.spring())
+                }
+            }
+                
+            if showDetail {
+                HikeDetail(hike: hike)
+                    //.transition(.slide)
+                    //.transition(.move(edge: .leading))
+                    .transition(.moveAndFade)
+            }
+        }
+    }
+}
+
+extension AnyTransition {
+    static var moveAndFade: AnyTransition {
+        let insertion = AnyTransition.move(edge: .trailing)
+            .combined(with: .opacity)
+        let removal = AnyTransition.scale
+            .combined(with: .opacity)
+        return .asymmetric(insertion: insertion, removal: removal)
     }
 }
 
 struct HikeView_Previews: PreviewProvider {
     static var previews: some View {
-        HikeView()
+        VStack {
+            HikeView(hike: hikeData[0])
+                .padding()
+
+            Spacer()
+        }
     }
 }
