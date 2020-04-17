c591ce2d95ff485ac6f65354624c17f6308420f1
appke <liangmu.gdut@gmail.com>
2020-04-17 17:11:03 +0800

    运行在模拟器

---
+++ Landmarks/CategoryHome.swift
@@ -0,0 +1,21 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by MGBook on 2020/4/17.
+//  Copyright © 2020 MGBook. All rights reserved.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
--- Landmarks/SceneDelegate.swift
+++ Landmarks/SceneDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SwiftUI
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -14,16 +15,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-
-        guard let _ = (scene as? UIWindowScene) else { return }
+        let contentView = LandmarkList().environmentObject(UserData())
         
-//        let window = UIWindow(frame: UIScreen.main.bounds)
-//        window.rootViewController = LandmarkList()
-//        self.window = window
-//        window.makeKeyAndVisible()
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(rootView: contentView)
+            self.window = window
+            window.makeKeyAndVisible()
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
